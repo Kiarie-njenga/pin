d72d2ea1905a5c411a2553e09ee8736334d89464
Kiarie_Njenga <jnnjenga030gmail.com>
2023-03-15 22:14:23 +0300

    'Fifth'

--- pins/models.py
+++ pins/models.py
@@ -4,7 +4,7 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from accounts.models import User
 from boards.models import Board
-
+from .validators import validate_file_size
 
 class Pin(models.Model):
     user = models.ForeignKey(
