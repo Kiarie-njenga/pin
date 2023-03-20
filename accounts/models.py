f75c64b2a7f0bb1f201a0319d6f27f6b39a95125
Kiarie_Njenga <jnnjenga030gmail.com>
2023-03-20 13:16:06 +0300

    'eleventh'

--- accounts/models.py
+++ accounts/models.py
@@ -53,3 +53,13 @@ class Follow(models.Model):
 
     def __str__(self):
         return f'{self.follower} is following {self.following}'
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
