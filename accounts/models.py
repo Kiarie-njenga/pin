52342a58ece2e6f8eabb44ca7c418168b5e6fc7c
Kiarie_Njenga <jnnjenga030gmail.com>
2023-03-20 13:32:39 +0300

    'twelve'

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser
-
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 from .managers import UserManager
 
 
